f1840371d3a9586c755331c9bb35034893ce4fa2
leonelcajal <leocajald@gmail.com>
2020-05-27 00:34:31 -0300

    update

---
+++ abm.py
@@ -0,0 +1,9 @@
+def registrarUsuarios(nombre,apellido,correoElec,dni,fechaDeNac,domicilio):
+    print("okey")
+def verificarContraCorrecta():
+    print("ok")
+
+def eliminarUnUsuario(dni):
+    print("ok")
+def registrarVentas():
+    print("ok")
